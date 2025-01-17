db179961d0b0b53022612971a047bd6278dc6170
Superior <samsonaderonmu2021@gmail.com>
2025-01-17 13:51:12 +0100

    remove unused import from Properties component

--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -1,4 +1,4 @@
-import { Search, SlidersHorizontal } from "lucide-react";
+import { SlidersHorizontal } from "lucide-react";
 import PropertyCard from "../components/PropertyCard";
 import { useState } from "react";
 import { Link } from "react-router-dom";
